88684474f66c7e6724bb9d66dbee0d263f0f091c
rilwanatoyebi <55920393+k9rado@users.noreply.github.com>
2023-05-15 11:35:25 +0100

    Update database for vendor.sql

--- database for vendor.sql
+++ database for vendor.sql
@@ -52,6 +52,24 @@ CREATE TABLE ratings (
   FOREIGN KEY (vendor_id) REFERENCES vendors (id)
 );
 
+CREATE TABLE `user_roles` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`role_name` VARCHAR(255) NOT NULL,
+	`user_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `order_products` ADD CONSTRAINT `order_products_fk0` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`);
+
+ALTER TABLE `order_products` ADD CONSTRAINT `order_products_fk1` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`);
+
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `user_roles` ADD CONSTRAINT `user_roles_fk0` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`);
+
+
+
+
 INSERT INTO vendors (name, contact_name, contact_email, contact_phone, website)
 VALUES ('Acme Corporation', 'John Smith', 'john.smith@acme.com', '123-456-7890', 'www.acme.com');
 
